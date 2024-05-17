99d5bee86145f8302518d9da0cc246a8ca053feb
Kévin Darcel <kevin.darcel@bitso.com>
2024-05-17 12:00:13 +0200

    feat: add enterprise license for MrRobot@fSociety
    - add build system using GHA

--- server/enterprise/external_imports.go
+++ server/enterprise/external_imports.go
@@ -5,6 +5,7 @@
 
 package enterprise
 
+/*
 import (
 	// Needed to ensure the init() method in the EE gets run
 	_ "github.com/mattermost/enterprise/account_migration"
@@ -51,3 +52,4 @@ import (
 	// Needed to ensure the init() method in the EE gets run
 	_ "github.com/mattermost/enterprise/intune"
 )
+*/
